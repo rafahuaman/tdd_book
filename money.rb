d504b48815d1af6c488a0eb3e6ec203d8f8f975d
Rafael Huaman <rlevano@thoughtworks.com>
2015-10-11 16:44:59 -0400

    finish chapter 3 and 4

--- money.rb
+++ money.rb
@@ -4,16 +4,25 @@ require 'purdytest'
 class MoneyTest < MiniTest::Unit::TestCase 
 	def	test_multiplication
 		five = Dollar.new(5)
-		product = five.times 2
-		assert_equal 10, product.amount
-		product = five.times 3
-		assert_equal 15, product.amount
+		assert_equal Dollar.new(10), five.times(2)
+		assert_equal Dollar.new(15), five.times(3)
+	end
+
+	def test_equality
+		assert Dollar.new(5).equals(Dollar.new(5))
+		refute Dollar.new(5).equals(Dollar.new(6))
+	end
+
+	def test_#==
+	  assert Dollar.new(5) == (Dollar.new(5))
+	  refute Dollar.new(5) == (Dollar.new(6))
 	end
 end
 
+
 class Dollar
-  attr_reader :amount
-  
+	attr_reader :amount
+
 	def initialize(amount)
 		@amount = amount
 	end
@@ -21,4 +30,9 @@ class Dollar
 	def times(multiplier)
 		Dollar.new(@amount*multiplier)
 	end
+
+	def ==(object)
+		@amount == object.amount
+	end
+	alias :equals :==
 end
