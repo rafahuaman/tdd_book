8ac79d4269334c8b8501cd35f5f00bd787db4662
Rafael Huaman <rlevano@thoughtworks.com>
2015-11-03 22:31:15 -0500

    Finish chapter 20

--- xunit/spec.py
+++ xunit/spec.py
@@ -8,27 +8,26 @@ class TestCase:
         self.setUp()
         method = getattr(self,self.name)
         method()
+        self.tearDown()
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self,name):
         TestCase.__init__(self,name)
 
     def testMethod(self):
-        self.wasRun= 1
+        self.log= self.log + "testMethod "
     def setUp(self):
-        self.wasRun= None
-        self.wasSetUp= 1
+        self.log= "setUp "
+    def tearDown(self):
+        self.log= self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
+    def testTemplateMethod(self):
         self.test= WasRun("testMethod")
-    def testRunning(self):
         self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp testMethod tearDown " == self.test.log )
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
